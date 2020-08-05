7c4b05767e3734a96e34893168e778565ab4de67
Lit3r4lly <literallynotf@gmail.com>
2020-08-06 01:52:18 +0300

    first and last commit tho (bad practice!)

---
+++ CliHandler.py
@@ -0,0 +1,80 @@
+import json
+
+TESTER_OVEN = """
+████████╗███████╗███████╗████████╗███████╗██████╗        ██████╗ ██╗   ██╗███████╗███╗   ██╗
+╚══██╔══╝██╔════╝██╔════╝╚══██╔══╝██╔════╝██╔══██╗      ██╔═══██╗██║   ██║██╔════╝████╗  ██║
+   ██║   █████╗  ███████╗   ██║   █████╗  ██████╔╝█████╗██║   ██║██║   ██║█████╗  ██╔██╗ ██║
+   ██║   ██╔══╝  ╚════██║   ██║   ██╔══╝  ██╔══██╗╚════╝██║   ██║╚██╗ ██╔╝██╔══╝  ██║╚██╗██║
+   ██║   ███████╗███████║   ██║   ███████╗██║  ██║      ╚██████╔╝ ╚████╔╝ ███████╗██║ ╚████║
+   ╚═╝   ╚══════╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝       ╚═════╝   ╚═══╝  ╚══════╝╚═╝  ╚═══╝ """
+
+class CliAppHandler:
+    def __init__(self) -> None:
+        print(TESTER_OVEN)
+        print("Hello, welcome to TesterOven. \nSimply testing system for executable files.")
+        print("All you need to do is to follow the instructions according to the chosen option in the next menu.")
+
+    def get_inputs(self) -> list:
+        """
+        This function gets the input for the tests
+        :return: list of the tested inputs
+        :rtype: list
+        """
+        i = 0
+        flag = True
+        inputs_list = []
+
+        print("\nEnter the inputs by order (One each time), and press ctrl+c when you want to stop insert the inputs.")
+        while flag:
+            try:
+                i += 1
+                tested_input = input(f"[#] Input {i}: ")
+                inputs_list.append(tested_input)
+
+            except KeyboardInterrupt:
+                print(f"[A{'' * len(tested_input)}\033[A") # clear last output line
+                print(f"\n[&] New tested inputs list: \n\t{inputs_list}")
+
+                flag = False
+
+        return inputs_list
+
+    def import_inputs(self, file_path: str) -> list:
+        """
+        This function import the tested inputs from the file
+        :return: list of the tested inputs
+        :rtype: list
+        """
+        with open(file_path, "r") as f:
+            file_content = f.read()
+            inputs_list = json.loads(file_content)
+            
+        return inputs_list
+        
+
+    def export_inputs(self, inputs: list, file_path: str):
+        """
+        This function export the tested inputs into file
+        :param inputs: The tested inputs for the testing
+        :inputs type: list
+        """
+        with open(file_path, "w") as f:
+            json.dump(inputs, f)
+
+        print(f"[&] Inputs has been expotred to : \n\t{file_path}")
+
+    def append_inputs(self, file_path: str) -> None:
+        """
+        This function append new inputs to the tested inputs list
+        :param inputs: The tested inputs for the testing
+        :inputs type: list
+        """
+        new_tested_inputs = self.get_inputs()
+        current_tested_inputs = self.import_inputs(file_path)
+
+        for tested_input in new_tested_inputs:
+            current_tested_inputs.append(tested_input)
+
+        self.export_inputs(current_tested_inputs, file_path)
+
+        return current_tested_inputs
---
+++ TesterOven.py
@@ -0,0 +1,44 @@
+# Created by Lit3r4lly
+import CliHandler
+import TesterHandler
+
+def manage_tester_oven() -> None:
+    """
+    This function manage the whole tester process
+    """
+    tester_handler = TesterHandler.TesterOvenHandler()
+    cli_handler = CliHandler.CliAppHandler()
+    user_choice = int(input("\n1 - Test inputs - Enter inputs, test your executable file and export the inputs and the outputs to files\n2 - Import test - Import old tested inputs according to option (1) from the inputs (json) file\n3 - Append inputs - Append new inputs to test to old tested inputs according to option (1) to the inputs (json) file\nYour choice: "))
+
+    if user_choice == 1:
+        inputs_list = cli_handler.get_inputs()
+        output = tester_handler.test_inputs(inputs_list)
+
+        user_choice = input("[;] Would you like to write the tested inputs to file for easy importing test section? (yes / no): ")
+        if user_choice == 'yes':
+            file_path = input("\n[#] Enter file path (json) for saving the tested inputs: ")
+            cli_handler.export_inputs(inputs_list, file_path)
+
+        tester_handler.write_output_to_file(output)
+
+    elif user_choice == 2:
+        file_path = input("\n[#] Enter inputs file path (json) for importing old tested inputs: ")
+        inputs_list = cli_handler.import_inputs(file_path)
+
+        output = tester_handler.test_inputs(inputs_list)
+        tester_handler.write_output_to_file(output)
+
+    elif user_choice == 3:
+        file_path = input("\n[#] Enter file path (json) for appending new inputs to old tested inputs: ")
+        inputs_list = cli_handler.append_inputs(file_path)
+
+        output = tester_handler.test_inputs(inputs_list)
+        tester_handler.write_output_to_file(output)
+
+    print("Finished.")
+
+def main():
+    manage_tester_oven()
+
+if __name__ == "__main__":
+    main()
